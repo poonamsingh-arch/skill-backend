4123afb42ab4d9940d17ac8f24490b3af9fecbd3
POONAM SINGH <poonams58589@gmail.com>
2026-03-26 20:59:27 +0530

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
-const MessageSchema = new mongoose.Schema({
-  text: String,
+const messageSchema = new mongoose.Schema({
+  username: String,
   sender: String,
+  text: String,
   createdAt: {
     type: Date,
     default: Date.now
   }
 });
 
-module.exports = mongoose.model("Message", MessageSchema);
+module.exports = mongoose.model("Message", messageSchema);
